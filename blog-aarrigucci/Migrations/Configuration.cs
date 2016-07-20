84a65d6e8a3b9310e26b295a41b50508729ab311
annette-arrigucci <annette.arrigucci@gmail.com>
2016-07-20 15:47:25 -0400

    created blog app

---
+++ blog-aarrigucci/Migrations/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using blog_aarrigucci.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace blog_aarrigucci.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<blog_aarrigucci.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(
+            new RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            var userManager = new UserManager<ApplicationUser>(
+           new UserStore<ApplicationUser>(context));
+            if (!context.Users.Any(u => u.Email == "annette.arrigucci@outlook.com"))
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "aarrigucci",
+                    Email = "annette.arrigucci@outlook.com",
+                    FirstName = "Annette",
+                    LastName = "Arrigucci",
+                    DisplayName = "Annette Arrigucci"
+                }, "password");
+            }
+            var userId = userManager.FindByEmail("annette.arrigucci@outlook.com").Id;
+            userManager.AddToRole(userId, "Admin");
+        }
+
+    }
+}
---
+++ blog-aarrigucci/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(blog_aarrigucci.Startup))]
+namespace blog_aarrigucci
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
